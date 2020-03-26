2c9446adcd0f12f798a363718988899360b54da2
Madeuss <mateus1two3@gmail.com>
2020-03-26 15:51:57 -0300

    App web frontend

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,28 +1,27 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    /* Listar todos os casos */
     async index(request, response) {
-        const { page = 1} = request.query;
-
-        const [count] = await connection('incidents').count();
-
-        const incidents = await connection('incidents')
-        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-        .limit(5)
-        .offset((page - 1) * 5)
-        .select([
-            'incidents.*', 
-            'ongs.name', 
-            'ongs.email', 
-            'ongs.whataspp', 
-            'ongs.city', 
-            'ongs.uf'
-        ]);
-        
-        response.header('X-Total-Count', count['count(*)']);
-
-        return response.json(incidents);
+       const { page = 1 } = request.query
+ 
+       const [count] = await connection('incidents').count()
+ 
+       const incidents = await connection('incidents')
+          .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+          .limit(5)
+          .offset((page - 1) * 5)
+          .select([
+             'incidents.*', 
+             'ongs.name', 
+             'ongs.email',
+             'ongs.whatsapp', 
+             'ongs.city', 
+             'ongs.uf'
+          ])
+ 
+       response.header('X-Total-Count', count['count(*)'])
+ 
+       return response.json(incidents)
     },
 
     /* Criar casos */
